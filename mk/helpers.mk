3f1cfc26de4b5da7f32519e7ab83843f61cc6f5f
Maksym Komar <komar@evologics.de>
2021-06-13 21:53:43 +0000

    Fix `make help` to show properly quotes in help

--- mk/helpers.mk
+++ mk/helpers.mk
@@ -4,6 +4,8 @@ space +=
 
 join-with = $(subst $(space),$1,$(strip $2))
 
-generate-help = sed -n -e 's/^\# help: \(.*\)/\1/;t z' -e b -e ':z' -e 'N;s/\(.*\)\n\([^:]*\):.*/printf "%-20s - %s\\n" "\2" "\1"/p' $1 | sh
+generate-help = sed -n -e 's/^\# help: \(.*\)/\1/;t z' -e b -e ':z' \
+	-e 'N;s/'\'/\'\\\\\'\'/g \
+	-e 's/\(.*\)\n\([^:]*\):.*/printf "%-20s - %s\\n" '\''\2'\'' '\''\1'\''/p' $1 | sh
 
 # vim: noexpandtab tabstop=4 shiftwidth=4 fileformat=unix
